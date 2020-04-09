476ef9627dc4f8b2c606ad22680845522f2e8ec5
Mishal Shah <shahmishal1998@gmail.com>
2020-04-09 17:51:13 +0530

    improved code quality

--- main.py
+++ main.py
@@ -1,65 +1,70 @@
-import libvirt
+"""
+    This script utilizes the python library of libvirt to display relevant information from the VMs.
+    The data displayed are:
+    - VM name
+    - the VM state
+    - allocated CPU number
+    - allocated and max memory
+    - disks and their source
+    - the list of network interfaces with their IP addresses
+"""
 import sys
-from constants import VIR_DOMAIN_EVENT_MAPPING, VIR_DOMAIN_STATE_MAPPING
 from xml.dom import minidom
-from prettytable import PrettyTable
-import logging
+import libvirt
+from constants import VIR_DOMAIN_EVENT_MAPPING, VIR_DOMAIN_STATE_MAPPING, KB_to_MB_FACTOR
+
 
 # registers default event implmenentation
 libvirt.virEventRegisterDefaultImpl()
 
 
-def diplay_info(conn, dom, event, detail, opaque):
-    if event == 2 or event == 5:
+def diplay_info(_conn, dom, event, _detail, _opaque):
+    """
+    displays the required information using the libvirt library
+    """
+    if event in (libvirt.VIR_DOMAIN_EVENT_STARTED, libvirt.VIR_DOMAIN_EVENT_STOPPED):
         print("")
         print("=-" * 25)
-        t = PrettyTable(['Name', 'Value'])
-        state, maxmem, mem, cpus, cput = dom.info()
-        t.add_row(['name', dom.name()])
-        t.add_row(['event', VIR_DOMAIN_EVENT_MAPPING.get(event, "?")])
-        t.add_row(['state', VIR_DOMAIN_STATE_MAPPING.get(state, "?")])
-        t.add_row(['cpus', str(cpus)])
-        t.add_row(['memory', str(mem*0.001)])
-        t.add_row(['max memory', str(maxmem*0.001)])
+        state, maxmem, mem, cpus, _cput = dom.info()
+        print("name: " + dom.name())
+        print("event: " + VIR_DOMAIN_EVENT_MAPPING.get(event, "?"))
+        print("state: " + VIR_DOMAIN_STATE_MAPPING.get(state, "?"))
+        print("cpus: " + str(cpus))
+        print("memory: " + str(mem * KB_to_MB_FACTOR))
+        print("max memory: " + str(maxmem * KB_to_MB_FACTOR))
 
         raw_xml = dom.XMLDesc(0)
         xml = minidom.parseString(raw_xml)
-        diskTypes = xml.getElementsByTagName('disk')
-        for diskType in diskTypes:
-            print('disk: type='+diskType.getAttribute('type')+' device='+diskType.getAttribute('device'))
-            diskNodes = diskType.childNodes
-            for diskNode in diskNodes:
-                if diskNode.nodeName[0:1] != '#' and diskNode.nodeName == "source":
-                    for attr in diskNode.attributes.keys():
-                        t.add_row(['disk: ' + diskNode.attributes[attr].name, diskNode.attributes[attr].value])
+        disk_types = xml.getElementsByTagName('disk')
+        for disk_type in disk_types:
+            print('disk: type='+disk_type.getAttribute('type') +
+                  ' device='+disk_type.getAttribute('device'))
+            disk_nodes = disk_type.childNodes
+            for disk_node in disk_nodes:
+                if disk_node.nodeName[0:1] != '#' and disk_node.nodeName == "source":
+                    for attr in disk_node.attributes.keys():
+                        print(
+                            'disk: ' + disk_node.attributes[attr].name
+                            + disk_node.attributes[attr].value)
 
-        print(t)
-        # interfaceTypes = xml.getElementsByTagName('interface')
-        # for interfaceType in interfaceTypes:
-        #     print('interface: type='+interfaceType.getAttribute('type'))
-        #     interfaceNodes = interfaceType.childNodes
-        #     for interfaceNode in interfaceNodes:
-        #         if interfaceNode.nodeName[0:1] != '#':
-        #             print(' '+interfaceNode.nodeName)
-        #             for attr in interfaceNode.attributes.keys():
-        #                 print(' '+interfaceNode.attributes[attr].name+' = '+interfaceNode.attributes[attr].value)
-
-        ifaces = dom.interfaceAddresses(libvirt.VIR_DOMAIN_INTERFACE_ADDRESSES_SRC_AGENT, 0)
-        print(ifaces)
+        ifaces = dom.interfaceAddresses(
+            libvirt.VIR_DOMAIN_INTERFACE_ADDRESSES_SRC_AGENT, 0)
         print("IP Address: ")
-        for (name, val) in ifaces.iteritems():
+        for (_, val) in ifaces.items():
             if val['addrs']:
                 for ipaddr in val['addrs']:
                     if ipaddr['type'] == libvirt.VIR_IP_ADDR_TYPE_IPV4:
                         print(ipaddr['addr'] + " VIR_IP_ADDR_TYPE_IPV4")
                     elif ipaddr['type'] == libvirt.VIR_IP_ADDR_TYPE_IPV6:
                         print(ipaddr['addr'] + " VIR_IP_ADDR_TYPE_IPV6")
-        
+
         print("=-" * 25)
 
 
 def register_events(conn):
-    # add callback to receive notifications of arbitary domain events occuring on a domain
+    """
+    add callback to receive notifications of arbitary domain events occuring on a domain
+    """
     conn.domainEventRegisterAny(
         None,
         libvirt.VIR_DOMAIN_EVENT_ID_LIFECYCLE,
@@ -68,14 +73,14 @@ def register_events(conn):
 
 
 # setup connection
-conn = libvirt.open("qemu:///system")
+CONN = libvirt.open("qemu:///system")
 
-if conn == None:
+if CONN is None:
     print('Failed to open connection to the hypervisor')
     sys.exit(1)
 
 # register events
-register_events(conn)
+register_events(CONN)
 
 # event loop
 while True:
